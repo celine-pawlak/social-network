97694387c08372658c55e08942e44382da752544
Martin-Bozon <62134476+Martin-Bozon@users.noreply.github.com>
2020-11-25 15:49:05 +0100

    définition attribut class comment

--- Class/Comment.php
+++ Class/Comment.php
@@ -6,5 +6,43 @@ namespace App\Database;
 
 class Comment
 {
+    private $_id;
+    private $_content;
+    private $_creation_date;
+    private $_user_id;
 
+    public function showComment()
+        {
+
+        }
+    
+    public function addComment()
+        {
+
+        }
+    
+    public function id()
+        {
+            return $this->_id;
+        }
+    public function content()
+        {
+            return $this->_content;
+        }
+    public function creation_date()
+        {
+            return $this->_creation_date;
+        }
+    public function user_id()
+        {
+            return $this->_user_id;
+        }
+    public function setId($id)
+        {
+            $this->_id = $id;
+        }   
+    public function setContent($content)
+        {
+            $this->_content = $content;
+        }            
 }
