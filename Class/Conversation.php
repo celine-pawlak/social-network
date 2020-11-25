9aa8bfc1563196c09066f604063f59363db38089
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-11-25 15:45:43 +0100

    Conversation's getters

--- Class/Conversation.php
+++ Class/Conversation.php
@@ -7,6 +7,16 @@ namespace App\Database;
 class Conversation
 {
   private $_id;
-  private $_IdCreator;
+  private $_idCreator;
   private $_name;
+
+  public function id(){
+    return $this->_id;
+  }
+  public function idCreator() {
+    return $this->_idCreator;
+  }
+  public function name() {
+    return $this->_name;
+  }
 }
