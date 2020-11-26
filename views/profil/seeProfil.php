e3033064fad7bfbf9397f94b69f3a0bd9a305b19
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-11-26 17:48:26 +0100

    Border-right in infos + center with grid from materialize

--- views/profil/seeProfil.php
+++ views/profil/seeProfil.php
@@ -1,18 +1,36 @@
-<img src="ressources/img/sunset.jpg" alt="Image de couverture" class="cover_img">
+<div class="cover_img"></div>
+
 
 <div class="container" id="seeProfil">
   <div class="center-align">
     <h1> Nom de l'user </h1>
-    <img src="ressources/img/talin.jpg" alt="Photo de profil" class="profile_img">
+    <img src="ressources/img/talin.jpg" alt="Photo de profil" class="profile_img circle">
   </div>
 
   <div class="row">
-    <div class="col s4 m4 background-lighter-grey z-depth-1">
-      <h2> Présentation </h2>
+    <div class="col s5 m5 background-lighter-grey z-depth-1" id="presentation_profile">
+      <h2 class="blue-text bold-text center-align"> Présentation </h2>
+      <p>Le texte de présentation devra être modifiable.</p>
     </div>
 
-    <div class="col s4 m4 offset-m2 background-lighter-grey z-depth-1">
-      <h2> Technologies maîtrisées </h2>
+    <div class="col s5 m5 offset-m2 background-lighter-grey z-depth-1 flex-row justify-content-spacearound">
+      <div class="information_profile" id="info_border">
+        <h2 class="blue-text bold-text"> Technologies </h2>
+        <ul>
+          <li>Tech 1</li>
+          <li>Tech 2</li>
+          <li>Tech 3</li>
+        </ul>
+      </div>
+      <div class="information_profile">
+        <h2 class="blue-text bold-text"> Hobbies </h2>
+        <ul>
+          <li>Hobby 1</li>
+          <li>Hobby 2</li>
+          <li>Hobby 3</li>
+        </ul>
+      </div>
+
     </div>
   </div>
   <div class="row">
