2325fca83f6b9b84daf7fd6b3f498baab1519be1
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-11-26 15:39:43 +0100

    HTML and first classes css

--- views/profil/seeProfil.php
+++ views/profil/seeProfil.php
@@ -1,1 +1,26 @@
-<p>Ceci est le HTML de voir un profil en particulier</p>
+<img src="ressources/img/sunset.jpg" alt="Image de couverture" class="cover_img">
+
+<div class="container" id="seeProfil">
+  <div class="center-align">
+    <h1> Nom de l'user </h1>
+    <img src="ressources/img/talin.jpg" alt="Photo de profil" class="profile_img">
+  </div>
+
+  <div class="row">
+    <div class="col s4 m4 background-lighter-grey z-depth-1">
+      <h2> Présentation </h2>
+    </div>
+
+    <div class="col s4 m4 offset-m2 background-lighter-grey z-depth-1">
+      <h2> Technologies maîtrisées </h2>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col s12 m12">
+      <h2> Créer une publication </h2>
+    </div>
+  </div>
+
+Afficher toutes les publications (class Post)
+
+</div>
