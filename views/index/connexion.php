b7fa26822abb2732a8f849e204e6e7ee518aa6e8
Maxime <maxime.siegl@laplateforme.io>
2020-12-08 10:31:44 +0100

    Update connexion.php

--- views/index/connexion.php
+++ views/index/connexion.php
@@ -1,7 +1,5 @@
-
 <div class="row">
     <form id="form_connexion" class="col s4 offset-s4 card">
-
         <div class="row">
             <div class="input-field col s8 offset-s2">
                 <input id="email" type="email" class="validate">
@@ -14,9 +12,11 @@
                 <label for="password">Mot De Passe</label>
             </div>
         </div>
-
         <button id="submit_ins" class="btn waves-effect waves-light col s5 offset-s4" type="submit" name="action">Se Connecter
             <i class="material-icons right">send</i>
         </button>
+        <div class="row">
+            <p>Pas encore inscrit ? <span id="inscription">Cliquez ici !!</span></p>
+        </div>
     </form>
 </div>
