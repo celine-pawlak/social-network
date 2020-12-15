ef6af8d8c3bff975089c5ae8b80e9f7f47918783
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-12-15 13:32:58 +0100

    suppression ' ligne 20

--- views/templates/header.php
+++ views/templates/header.php
@@ -2,7 +2,7 @@
 <div class="row">
     <div class="col s12">
         <div class="row">
-            <div class="input-field col s12">                
+            <div class="input-field col s12">
                 <input type="text" id="autocomplete-input" class="autocomplete">
                 <label for="autocomplete-input">Autocomplete</label>
                 <i class="material-icons prefix">search</i>
@@ -17,10 +17,10 @@
     <a class='dropdown-trigger' href='#' data-target='dropdown1'><i class="fas fa-chevron-down"></i></a>
     <ul id='dropdown1' class='dropdown-content'>
         <!-- Changer les liens -->
-        <li><a href="profil?id=<?= $_SESSION['user']['id'] ?>'" id="profil_h">Profil</a></li>
+        <li><a href="profil?id=<?= $_SESSION['user']['id'] ?>" id="profil_h">Profil</a></li>
         <li><a href="messagerie" id="message_h">Messagerie</a></li>
         <li class="divider" tabindex="-1"></li>
-        <li><a href="modifier_profil" id="modif_h">Modifier profil</a></li>        
+        <li><a href="modifier_profil" id="modif_h">Modifier profil</a></li>
     </ul>
     <i class="fas fa-power-off"></i>
 </div>
