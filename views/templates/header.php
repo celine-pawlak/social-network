c9b01c6edbd2b4ec0dafe13a46719a5a06340b28
Martin-Bozon <62134476+Martin-Bozon@users.noreply.github.com>
2020-12-15 16:37:31 +0100

    je sais plus

--- views/templates/header.php
+++ views/templates/header.php
@@ -1,8 +1,8 @@
 <h4 class="blue-text" id="titre_header">Social Network</h4>
 
-<section id="profil_header" class="<?= isset($_SESSION['user']['id']) ? '' : 'd-none'?>">
+<section id="connect_header" class="<?= isset($_SESSION['user']['id']) ? '' : 'd-none'?>">
     <!-- Barre de recherche -->
-    <div class="row m-0">
+    <div class="row" id="barre_recherche">
         <div class="col s12">
             <div class="row">
                 <div class="input-field col s12">                
@@ -14,16 +14,16 @@
         </div>
     </div>
     <!-- Partie profil -->
-    <div>
+    <div id="profil_header">
         <!-- Image de profil -->    
         <img src="<?= $_SESSION['user']['picture_profil'] ?>" alt="photo de profil" id="pp_header">        
         <!-- Menu dropdown -->
-        <a class='dropdown-trigger' href='#' data-target='dropdown1'><i class="fas fa-chevron-down"></i></a>
+        <a class='dropdown-trigger' href='#' data-target='dropdown1'><i class="fas fa-chevron-down blue-text"></i></a>
         <ul id='dropdown1' class='dropdown-content'>            
-            <li><a href="profil?id=<?= $_SESSION['user']['id'] ?>" id="profil_h">Profil</a></li>
-            <li><a href="messagerie" id="message_h">Messagerie</a></li>
+            <li><a href="profil?id=<?= $_SESSION['user']['id'] ?>">Profil</a></li>
+            <li><a href="messagerie">Messagerie</a></li>
             <li class="divider" tabindex="-1"></li>
-            <li><a href="modifier_profil" id="modif_h">Modifier profil</a></li>        
+            <li><a href="modifier_profil">Modifier profil</a></li>        
         </ul>
         <i class="fas fa-power-off"></i>
     </div>
