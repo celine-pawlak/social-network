0086ec07f13a2e80c2c89f18064bc4e4781acc15
Martin-Bozon <62134476+Martin-Bozon@users.noreply.github.com>
2020-12-16 10:55:57 +0100

    css header mode normal ok

--- views/templates/header.php
+++ views/templates/header.php
@@ -1,22 +1,21 @@
 <h4 class="blue-text" id="titre_header">Social Network</h4>
 
-<section id="connect_header" class="<?= isset($_SESSION['user']['id']) ? '' : 'd-none'?>">
+<section id="connect_header" class="<?= isset($_SESSION['user']['id']) ? 'd-flex' : 'd-none'?>">
     <!-- Barre de recherche -->
     <div class="row" id="barre_recherche">
         <div class="col s12">
-            <div class="row">
-                <div class="input-field col s12">                
-                    <input type="text" id="autocomplete-input" class="autocomplete">
-                    <label for="autocomplete-input">Autocomplete</label>
-                    <i class="material-icons prefix">search</i>
+            <div class="row m-0">
+                <div class="input-field col s12 m-0">                
+                    <input type="text" id="autocomplete-input" class="autocomplete" placeholder="Rechercher une personne...">                    
+                    <!-- <i class="material-icons prefix">search</i> -->
                 </div>
             </div>
         </div>
     </div>
     <!-- Partie profil -->
     <div id="profil_header">
-        <!-- Image de profil -->    
-        <img src="<?= $_SESSION['user']['picture_profil'] ?>" alt="photo de profil" id="pp_header">        
+        <!-- Image de profil -->          
+        <img src="<?= isset($_SESSION['user']) ? $_SESSION['user']['picture_profil'] : '' ?>" alt="photo de profil" id="pp_header">        
         <!-- Menu dropdown -->
         <a class='dropdown-trigger' href='#' data-target='dropdown1'><i class="fas fa-chevron-down blue-text"></i></a>
         <ul id='dropdown1' class='dropdown-content'>            
