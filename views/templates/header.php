4ee456417c010fb6ba0b518f1713c740f6086310
Martin-Bozon <62134476+Martin-Bozon@users.noreply.github.com>
2020-12-18 11:20:31 +0100

    encore un peu de responsive

--- views/templates/header.php
+++ views/templates/header.php
@@ -30,16 +30,8 @@
 <!-- Responsive -->
 <a href="#" id="ilestpasdedans" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
 <ul id="slide-out" class="sidenav">
-        <li>
-            <div class="user-view">      
-                <img src="<?= isset($_SESSION['user']) ? $_SESSION['user']['picture_profil'] : '' ?>" alt="photo de profil" id="pp_header">
-                <span class="name"><?= $_SESSION['user']['first_name'] ?></span>
-                <span class="email"><?= $_SESSION['user']['mail'] ?></span>
-            </div>    
-        </li> 
-        <li class="divider" tabindex="-1"></li>
         <div class="row" id="barre_recherche">
-            <div class="col l12 m12 s4">
+            <div class="col s12">
                 <div class="row m-0">
                     <div class="input-field col s12 m-0">                
                         <input type="text" id="autocomplete-reponsive" class="autocomplete" placeholder="Rechercher une personne...">                    
@@ -48,6 +40,14 @@
                 </div>
             </div>
         </div>
+        <li class="divider" tabindex="-1"></li>
+        <li>
+            <div class="user-view">      
+                <img src="ressources/img/<?= $_SESSION['user']['picture_profil'] ?>" alt="photo de profil" id="pp_header">
+                <span class="name"><?= $_SESSION['user']['first_name'] ?></span>
+                <span class="email"><?= $_SESSION['user']['mail'] ?></span>
+            </div>    
+        </li>                
         <li class="divider" tabindex="-1"></li>
         <li><a href="profil?id=<?= $_SESSION['user']['id'] ?>">Profil</a></li>
         <li><a href="messagerie">Messagerie</a></li>
