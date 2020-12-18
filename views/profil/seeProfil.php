6aec29614e9c59ee629338c779bdcaaaea32f13c
Cécile <62134503+cecile-wojnowski@users.noreply.github.com>
2020-12-18 15:22:16 +0100

    chemin absolu avec const URL

--- views/profil/seeProfil.php
+++ views/profil/seeProfil.php
@@ -68,7 +68,7 @@
         <?php foreach($posts as $post): ?>
           <?php //var_dump($post); ?>
           <div class="col s1 m1">
-            <img class="circle miniature_img" src="<?= "ressources/img/". $post['picture_profil'] ?>" alt="Photo de profil">
+            <img class="circle miniature_img" src="<?= URL . "ressources/img/". $post['picture_profil'] ?>" alt="Photo de profil">
             <p><?= $post["first_name"] . " " .$post["last_name"] ?></p>
           </div>
 
