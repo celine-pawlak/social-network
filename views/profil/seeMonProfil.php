b526342bfd193320ec7475516134bfae31bb95f5
Maxime Siegl <maxime.siegl@laplateforme.io>
2021-01-17 16:36:18 +0100

    Css mon profil ok

--- views/profil/seeMonProfil.php
+++ views/profil/seeMonProfil.php
@@ -55,8 +55,8 @@
   </div>
   <div class="row">
     <div class="col s10 m10 offset-m1">
-      <form class="form_profile p-2 background-lighter-grey z-depth-1" action="addPostForm" method="post">
-        <textarea class="background-lighter-grey" name="post" placeholder=" Ecrire une publication..."></textarea>
+      <form class="form_profile p-2 background-lighter-grey z-depth-1" action="addPostForm" method="POST">
+        <textarea class="background-lighter-grey" id="post_profil" name="post_profil" placeholder=" Ecrire une publication..."></textarea>
         <button class="btn-floating waves-effect waves-light" type="submit" id="add_post_profil"><i class="material-icons">send</i></button>
       </form>
     </div>
@@ -72,16 +72,16 @@
             <p class="bold-text"><?= $post["first_name"] . " " .$post["last_name"] ?></p>
           </div>
 
-          <div class="col s9 m9 offset-m1 z-depth-1 background-lighter-grey m-1">
-            <p><?= $post['13'] ?> </p>
-            <div class="post post_profile p-1 z-depth-1">
+          <div class="flex-column relative background-lighter-grey border-radius-25px all_posts mx-auto content-fit-height box-shadow my-1">
+            <p class="ml-1 mt-05 mb-1 grey-text font-smile-small"><?= $post['13'] ?> </p>
+            <div class="post post_profile border-radius-10px box-shadow background-blue-grey black-text mx-1 p-1">
               <p class="right-align">
                 <i id="fa-heart" class="fas fa-heart"></i>
                 <i id="fa-thumbs-up" class="fas fa-thumbs-up"></i>
                 <i id="fa-laugh-squint" class="fas fa-laugh-squint"></i>
                 <i id="fa-sign-language" class="fas fa-sign-language"></i>
               </p>
-              <p id="<?= "posts_" . $post['0'] ?>" class="post_content">
+              <p id="<?= "posts_" . $post['0'] ?>" class="post_content m-0 font-normal">
                 <?= $post["content"] ?>
 
                 <?php foreach($reacts as $react):
@@ -94,27 +94,43 @@
             </div>
 
             <?php if(!empty($commentaires["post_".$post['0']])): ?>
-            <div id="commentaires_post_<?= $post['0'] ?>">
-            <?php foreach($commentaires["post_".$post['0']] as $commentaire): ?>
-              <div class="post comment_profile p-1 z-depth-1">
-                <div class="flex-row">
-                  <img class="circle miniature_img" src="<?= URL.'/ressources/img/'. $commentaire['picture'] ?>">
-                  <p class="bold-text ml-05"><?= $commentaire["first_name"] . " " . $commentaire["last_name"] ?></p>
-                </div>
+              <div class="messages_show_or_hide">
+                        <p class="clickable grey-text mb-1 font-smile-small text-right mx-1 mt-0 show-comments" id="see_comments_from_post_<?= $post['0'] ?>">Voir
+                            les commentaires (<?= count($commentaires["post_" . $post['0']]) ?>) <i class="fas fa-chevron-down"></i></p>
+                        <p class="clickable grey-text mb-1 font-smile-small text-right mx-1 mt-0 hide-comments" id="hide_comments_from_post_<?= $post['0'] ?>">Cacher
+                            les commentaires <i class="fas fa-chevron-up"></i></p>
 
-                <p><?= $commentaire["comment"] ?></p>
-                <p class="grey-text right-align"><?= $commentaire["date"] ?></p>
+                    </div>
+
+            <div id="commentaires_post_<?= $post['0'] ?>" class="mb-1 px-05 self-align-flexend w-90 commentaires_posts">
+            <?php foreach($commentaires["post_".$post['0']] as $commentaire): ?>
+              <div class="post comment_profile border-radius-10px box-shadow background-white black-text m-1 p-05 flex-row  ml-1">
+                <img class="circle miniature_img" src="<?= URL.'/ressources/img/'. $commentaire['picture'] ?>">  
+                <div class="flex-1 flex-column">
+                  <div class="flex-row justify-content-spacebetween">
+                    <p class="bold-text font-smile-small pl-05 m-0"><?= $commentaire["first_name"] . " " . $commentaire["last_name"] ?></p>
+                    <p class="grey-text right-align font-smile-small m-0"><?= $commentaire["date"] ?></p>
+                  </div>
+                  <p class="pl-05 m-0 content-fit-height word-break-all"><?= $commentaire["comment"] ?></p>
+                </div> 
               </div>
             <?php endforeach; ?>
           </div>
           <?php endif; ?>
 
-            <form class = "form_comment center-align" method="post">
-              <input type="hidden" name="id_user" value="<?= $id_user ?>">
-              <input type="hidden" name="id_post" value="<?= $post['0'] ?>">
-              <input type="text" name="content" value="">
-              <input type="submit" class="btn" name="" value="Commenter">
-            </form>
+          <form class="form_comment center-align px-1 flex-row self-align-flexend w-90" method="post">
+                    <div class="flex-1">
+                        <input type="hidden" name="id_user" value="<?= $id_user ?>">
+                        <input type="hidden" name="id_post" value="<?= $post['0'] ?>">
+                        <textarea id="comment_post_<?= $post['0'] ?>" class="py-05 w-90 no-border mx-1 no-resize"
+                                  type="text" name="content" placeholder="Commenter..."
+                                  onkeyup="textAreaAdjust(this)"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-small btn-floating waves-effect waves-light">
+                        <input type="hidden" name="" value="Commenter">
+                        <i class="material-icons btn" id="add_post">send</i>
+                    </button>
+                </form>
           </div>
         <?php endforeach; ?>
 
